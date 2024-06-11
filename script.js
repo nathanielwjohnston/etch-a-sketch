494ef2f1b38dcade95f69d3e3a856851ce8adc99
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-11 22:32:52 +0100

    Add class to new divs

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ container = document.querySelector(".container");
 
 for (i = 0; i < 16; i++) {
     newDiv = document.createElement("div");
+    newDiv.classList.add("grid-square");
     container.appendChild(newDiv);
 }
