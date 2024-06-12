9cd7ae9376d2d07c1f21104357af9a71ba7358e3
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-12 22:41:08 +0100

    Add drawing effect when hovering grid squares

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ for (i = 0; i < 16; i++) {
     newDiv.classList.add("grid-square");
     container.appendChild(newDiv);
 }
+
+// mouseover bubbles
+container.addEventListener("mouseover", (e) => {
+    if (!e.target.classList.contains("hovering")) {
+        e.target.classList.toggle("hovering");
+    }
+})
