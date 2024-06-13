8625965fff4c6030ef0d2d83182cb0e060e562c2
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-13 22:04:26 +0100

    Add grid-change functionality

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 // Create grid
 container = document.querySelector(".container");
 
-for (i = 0; i < 16; i++) {
-    newDiv = document.createElement("div");
-    newDiv.classList.add("grid-square");
-    container.appendChild(newDiv);
+function createGrid(numberOfSquares) {
+    for (i = 0; i < numberOfSquares; i++) {
+        for (j = 0; j < numberOfSquares; j++) {
+            newDiv = document.createElement("div");
+            newDiv.classList.add("grid-square");
+            container.appendChild(newDiv);
+        }
+    }
+}
+
+function clearGrid() {
+    gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach(square => {
+        square.remove();
+    })
 }
 
 // mouseover bubbles
 container.addEventListener("mouseover", (e) => {
-    if (!e.target.classList.contains("hovering")) {
+    if (!e.target.classList.contains("hovering") && !e.target.classList.contains("container")) {
         e.target.classList.toggle("hovering");
     }
 })
 
 button = document.querySelector("button");
 button.addEventListener("click", () => {
+    let gridSize = null
     while (true) {
-        let gridSize = prompt("Enter grid size");
+        gridSize = prompt("Enter grid size");
         if (gridSize <= 100) {
             break;
         }
     }
+    clearGrid();
+    createGrid(gridSize);
+    let basis = 100 / gridSize;
+    gridSquares = document.querySelectorAll(".grid-square");
+    console.log(gridSquares);
+    gridSquares.forEach(square => {
+        square.style.flexBasis = `${basis}%`;  
+    });
 })
